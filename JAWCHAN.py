eb95544d138b1eb303785dffc633bce9de0e3c9f
KTAISA <iblrecords@gmail.com>
2014-06-25 19:10:03 -0500

    V1

--- JAWCHAN.py
+++ JAWCHAN.py
@@ -3,6 +3,7 @@ __author__ = 'JAWASSMACPRO'
 #main 4chan API app
 
 import requests
+from bs4 import BeautifulSoup
 
 
 def boardLIST():
@@ -15,23 +16,54 @@ def boardLIST():
         genre = bLIST[i]
         print('Board Name: {} | /{}' .format(genre['title'], genre['board']))
 
+    menu()
+
 def boardPOSTS():
-    base_url = 'https://a.4cdn.org/g/1.json'
-    jawchan = requests.request('get', base_url).json()
+    base_url = 'https://a.4cdn.org/'
+    boardPICK = str(input('Pick a board: '))
+    final = base_url + boardPICK + '/1.json'
+    jawchan = requests.request('get', final).json()
 
     #print(jawchan)
     threads = jawchan['threads']
     #print(threads)
-    print(len(threads))
+    #print(len(threads))
 
     for i in range(len(threads)):
         #print(threads[i])
         POSTS = threads[i]['posts']
-
+        print('---------------------------------------------------------------------------------')
         print(POSTS[0]['no'])
         print(POSTS[0]['now'])
-        print(POSTS[0]['com'])
-        print(POSTS[0]['replies'])
+        if 'com' in POSTS[0]:
+            post = POSTS[0]['com']
+            soup = BeautifulSoup(post)
+            print(soup.get_text())
+        print('Thread replies - {}' .format(POSTS[0]['replies']))
+
+    menu()
+
+def threadPOSTS():
+    boardPICK = str(input('Pick a board: '))
+    threadNUM = str(input('Pick Thread Number: '))
+    base_url = 'https://a.4cdn.org/'
+    final = base_url + boardPICK + '/thread/' + threadNUM + '.json'
+    jawchan = requests.request('get', final).json()
+    #print(jawchan)
+
+    fullTHREAD = jawchan['posts']
+
+    for i in range(len(fullTHREAD)):
+        POSTS = fullTHREAD[i]
+        print('----------------------------------------------------------------------------------')
+        print('{} | {} ' .format(POSTS['no'], POSTS['now']))
+
+        if 'com' in POSTS:
+            post = POSTS['com']
+            soup = BeautifulSoup(post)
+            print(soup.get_text())
+
+    menu()
 
 def menu():
     print('------------------------------------------------')
